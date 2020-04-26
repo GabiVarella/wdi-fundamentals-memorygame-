92df709c7de2923e5ed970c5cf19d21a075f68f8
Gabriel Varella <gabrielvarella@Gabriels-MacBook-Pro.local>
2020-04-25 19:07:46 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -4,25 +4,30 @@ let cards = ["queen", "king", "queen",  "king"];
 
 let cardsInPlay = [];
 
-let cardOne = cards[0];
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");} 
+else {
+  console.log("Sorry, try again.");
+}
+}
 
-cardsInPlay.push(cardOne);
+function flipCard (cardId) 
 
-console.log("User flipped " + cardOne);
+{
 
-let cardTwo = cards[2];
+console.log("User flipped " + cards[cardId]);
 
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardTwo);
+cardsInPlay.push(cards[cardId]);
 
 
 if (cardsInPlay.length === 2) {
 	
-	 if (cardsInPlay[0] === cardsInPlay[1])
-
-	 {alert("You found a match!");}
-
-	else {alert("Sorry, try again.")}
+	checkForMatch(); 
 
 }
+
+
+
+}
+
